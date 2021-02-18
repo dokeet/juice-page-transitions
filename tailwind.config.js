ebb6c3c7a1082bc0e20b00c1fb4c787ec813876c
Jorge <jorgecbll@gmail.com>
2021-02-18 17:06:44 -0300

    extending colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts}', './components/**/*.{js,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      lime: colors.lime,
-      yellow: colors.yellow,
-      green: colors.green
-    }
+    extend: {
+      colors: {
+        lime: colors.lime,
+        yellow: colors.yellow,
+        green: colors.green
+      }
+    },
+
   },
   variants: {
     extend: {},
